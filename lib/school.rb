b27ab0ba382b4e62f6004b3842df7a25806e0cc5
noisegaze <ben.stephanus@gmail.com>
2021-01-28 16:25:28 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+
+require 'pry'
+
+class School 
+
+    attr_accessor :school_name, :student
+    attr_reader :roster 
+
+    def initialize(school_name)
+        @school_name = school_name 
+        @roster = {}
+    end
+
+    def add_student(name,grade)
+        grade = grade.to_i
+        @roster[:grade]=[]
+        @roster[:grade] << name
+    end
+    
+
+
+end
